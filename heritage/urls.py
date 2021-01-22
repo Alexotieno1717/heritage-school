87b1a4f2424223383e32b30c002fb2102f98867a
Alexotieno1717 <alexotieno900@gmail.com>
2021-01-22 18:34:59 +0300

    linked the pages in urls.py

--- heritage/urls.py
+++ heritage/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('curriculum/', views.curriculum, name='curriculum'),
+    path('news/', views.news, name='news'),
+    path('awwards/', views.awwards, name='awwards'),
+    path('give/', views.give, name='give'),
+    path('contact/', views.contact, name='contact'),
 ]
--- heritage/views.py
+++ heritage/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'heritage/index.html')
+
+def about(request):
+    return render(request, 'heritage/about.html')
+
+def curriculum(request):
+    return render(request, 'heritage/curriculum.html')
+
+def news(request):
+    return render(request, 'heritage/news.html')
+
+def awwards(request):
+    return render(request, 'heritage/awwards.html')
+
+def give(request):
+    return render(request, 'heritage/give.html')
+
+def contact(request):
+    return render(request, 'heritage/contact.html')
